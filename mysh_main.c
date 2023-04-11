6b78eb5eba218313d1c8c8541b9c5f59c2dcbdfe
Alex Galanos <82150082+AlexGlns@users.noreply.github.com>
2023-04-11 22:27:31 +0300

    Add files via upload

--- mysh_main.c
+++ mysh_main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <string.h>
 
-#define LENGTH 1024
+#define LENGTH 1024 // max length of command
+#define PERMS 0644  // set access permissions
 
-char *commandParser(char *, char **);
+char *commandParser(char *, char **, int *);
 
 int main()
 {
@@ -14,7 +16,7 @@ int main()
 
     while (1)
     {
-        int status;
+        int status, fd = -1;
         char command[LENGTH], ch; // input can be at most 100 chars
         printf("in-mysh-now:>");
 
@@ -39,17 +41,19 @@ int main()
             continue;
         }
 
-        //char *parsedCmd[strlen(command) + 1];
+        // char *parsedCmd[strlen(command) + 1];
         char **parsedCmd = NULL;
-        parsedCmd =(char **) malloc(sizeof(char*) * strlen(command)+1);
-        for(int j=0;j<strlen(command)+1;j++){
-            parsedCmd[j]=malloc(sizeof(char) * strlen(command)+1);
+        parsedCmd = (char **)malloc(sizeof(char *) * strlen(command) + 1);
+        for (int j = 0; j < strlen(command) + 1; j++)
+        {
+            parsedCmd[j] = malloc(sizeof(char) * strlen(command) + 1);
         }
-        *parsedCmd = commandParser(command, parsedCmd);
-        printf("%s %s\n", parsedCmd[0], parsedCmd[1]);
+        //*parsedCmd = commandParser(command, parsedCmd, &fd);
+        // printf("%s %s\n", parsedCmd[0], parsedCmd[1]);
 
         if (pid == 0)
-        { // child process
+        {                                                        // child process
+            *parsedCmd = commandParser(command, parsedCmd, &fd); // parse command
 
             execvp(parsedCmd[0], parsedCmd);
             perror("execvp");
@@ -59,31 +63,61 @@ int main()
         { // parent process is mysh shell
             wait(&status);
         }
-        
-        //free parsedStr
-        for(int j=0;j<strlen(command)+1;j++){
+
+        // free parsedStr
+        for (int j = 0; j < strlen(command) + 1; j++)
+        {
             free(parsedCmd[j]);
+        }
+        if (fd != -1)
+        {
+            close(fd);
         }
         free(parsedCmd);
     }
 }
 
-char *commandParser(char *cmd, char **cmdParsed)
+char *commandParser(char *cmd, char **cmdParsed, int *filedesc)
 {
     // char cmdParsed[strlen(cmd)+1][strlen(cmd)+1];
     char delim[] = " ";
     char *word;
+    // int createFile = 0; // int variable 1 or 0 to know if we need to open a file
 
     word = strtok(cmd, delim);
     int counter = 0;
     while (word != NULL)
     {
+        if (strcmp(word, ">") == 0)
+        {
+            word = strtok(NULL, delim);
+            while (word != NULL)
+            {
+                // printf("in while loop \n");
+                if ((*filedesc = open(word, O_CREAT | O_RDWR, PERMS)) == -1)
+                {
+                    perror("creating");
+                }
+                dup2(*filedesc, 1);
+                dup2(*filedesc, 2);
+                word = strtok(NULL, delim);
+            }
+            //cmdParsed[counter] = NULL;
+            //printf("returning \n");
+            //return *cmdParsed;
+
+            break;
+        }
+        //printf("1 \n");
         strcpy(cmdParsed[counter], word);
         word = strtok(NULL, delim);
+        //printf("%s  \n", word);
+
         counter++;
     }
 
     cmdParsed[counter] = NULL;
+    //printf("%s %s\n", cmdParsed[0], cmdParsed[1]);
 
     return *cmdParsed;
 }
