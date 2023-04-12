24f499a1d27b0fb6e73ea6184a40dbcbda1a5d58
Alex Galanos <82150082+AlexGlns@users.noreply.github.com>
2023-04-12 13:41:14 +0300

    Add files via upload
    Adds Pipes Feature(alpha)

--- mysh_main.c
+++ mysh_main.c
@@ -4,11 +4,12 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <string.h>
-
+#define READ 0      // read end for read
+#define WRITE 1     // write end for pipe
 #define LENGTH 1024 // max length of command
 #define PERMS 0644  // set access permissions
 
-char *commandParser(char *, char **, int *);
+char *commandParser(char *, char **, int *, int *);
 int checkSpecialChars(char *);
 
 int main()
@@ -16,8 +17,12 @@ int main()
     pid_t pid;
 
     while (1)
-    {
-        int status, fd = -1;      // fd = file descriptor for potentially opening a file
+    {   
+        /*
+          fd = file descriptor for potentially opening a file
+          p = 1 if a pipe found in command
+        */
+        int status, fd = -1, p=0;      
         char command[LENGTH], ch; // input can be at most 100 chars
         printf("in-mysh-now:>");
 
@@ -54,8 +59,10 @@ int main()
 
         if (pid == 0)
         {                                                        // child process
-            *parsedCmd = commandParser(command, parsedCmd, &fd); // parse command
-
+            *parsedCmd = commandParser(command, parsedCmd, &fd, &p); // parse command
+            if(p == 1){
+                exit(1);
+            }
             execvp(parsedCmd[0], parsedCmd);
             perror("execvp");
             exit(1);
@@ -70,15 +77,18 @@ int main()
         {
             free(parsedCmd[j]);
         }
+
+        free(parsedCmd);
+
+        // close File Descriptor
         if (fd != -1)
         {
             close(fd);
         }
-        free(parsedCmd);
     }
 }
 
-char *commandParser(char *cmd, char **cmdParsed, int *filedesc)
+char *commandParser(char *cmd, char **cmdParsed, int *filedesc, int *p)
 {
     char delim[] = " ";
     char *word;
@@ -87,7 +97,7 @@ char *commandParser(char *cmd, char **cmdParsed, int *filedesc)
     int counter = 0;
     while (word != NULL)
     {
-        if (strcmp(word, ">") == 0)     // stdout redirection
+        if (strcmp(word, ">") == 0) // stdout redirection
         {
             word = strtok(NULL, delim);
             if (word == NULL)
@@ -103,7 +113,7 @@ char *commandParser(char *cmd, char **cmdParsed, int *filedesc)
                 perror("creating");
             }
             dup2(*filedesc, 1);
-            // dup2(*filedesc, 2);
+
             word = strtok(NULL, delim);
             //}
             if (word == NULL)
@@ -111,8 +121,9 @@ char *commandParser(char *cmd, char **cmdParsed, int *filedesc)
                 break;
             }
         }
-        
-        if(strcmp(word, "<") == 0) {   //stdin redirection
+
+        if (strcmp(word, "<") == 0)
+        { // stdin redirection
             word = strtok(NULL, delim);
             if (word == NULL)
             {
@@ -131,8 +142,8 @@ char *commandParser(char *cmd, char **cmdParsed, int *filedesc)
                 break;
             }
         }
-        
-        if (strcmp(word, ">>") == 0)   //append to end of file
+
+        if (strcmp(word, ">>") == 0) // append to end of file
         {
             word = strtok(NULL, delim);
             if (word == NULL)
@@ -152,7 +163,61 @@ char *commandParser(char *cmd, char **cmdParsed, int *filedesc)
                 break;
             }
         }
-        if (checkSpecialChars(word)){
+
+        if (strcmp(word, "|") == 0) {      //execute pipe
+            *p=1;
+            
+            //counter++;
+            cmdParsed[counter] = NULL;
+
+            word = strtok(NULL, delim);
+            if (word == NULL) {
+                strcpy(cmdParsed[counter], "|");
+                counter++;
+                break;
+            }
+            //store second command
+            char* cmd1[strlen(cmd)+1];
+            //int counter1 =0;
+
+            // find command write of pipe
+            // while (word != NULL)
+            // {
+            //     strcpy(cmd1[counter], word);
+            //     word = strtok(NULL, delim);
+            //     counter1++;
+            // }
+            *cmd1 = commandParser(word,cmd1,filedesc,p);
+            printf("%s  %s\n",cmd1[0],cmd1[1]);
+            printf("%s  %s\n",cmdParsed[0],cmdParsed[1]);
+
+
+            int fds[2]; // file desc to open Pipe
+            pipe(fds);
+            pid_t pid = fork();
+            if (pid == -1){
+                perror("fork");
+            }
+
+            if (pid == 0){  //child execute command after pipe
+                close(fds[WRITE]);
+                dup2(fds[READ],0);
+                execvp(cmd1[0],cmd1);
+                perror("exec1");
+                //return -1;
+            }else{
+                close(fds[READ]);
+                dup2(fds[WRITE],1);
+                execvp(cmdParsed[0],cmdParsed);
+                perror("exec2");
+                //return -1;
+            }
+
+            break;
+        }
+
+        if (checkSpecialChars(word))
+        {
             continue;
         }
         // printf("1 \n");
@@ -169,10 +234,13 @@ char *commandParser(char *cmd, char **cmdParsed, int *filedesc)
     return *cmdParsed;
 }
 
-int checkSpecialChars(char *wd){
-    char* spChars[] = {">","<",">>"};
-    for (int i=0;i<3;i++) {
-        if(strcmp(wd,spChars[i])==0){
+int checkSpecialChars(char *wd)
+{
+    char *spChars[] = {">", "<", ">>"};
+    for (int i = 0; i < 3; i++)
+    {
+        if (strcmp(wd, spChars[i]) == 0)
+        {
             return 1;
         }
     }
